cda4819e8f468ce9f47f7202c61c1bde44360de8
Tony <tony.brechard@epitech.eu>
2024-02-26 17:23:08 +0100

    Journée fini

--- includes/profil.php
+++ includes/profil.php
@@ -1,39 +1,24 @@
-<!DOCTYPE html>
-<html>
 
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Twitter Profile</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='../style/profil.css'>
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-
-<body>
     <div class="flexcontainer">
         <div class="middlecontainer">
             <section class="headsec">
-                <div>
-                    <h3>Username</h2>
-                        <span>Compte Tweets</span>
-                </div>
             </section>
             <section class="twitterprofile">
                 <div class="headerprofileimage">
-                    <div id="headerimage">Bannière du mec</div>
-                        <div id="profilepic">Pp du mec</div>
-                    <div class="editprofile">Edit Profile</div>
+                    <img id="headerimage" src="https://res.cloudinary.com/dowrygm9b/image/upload/v1570267399/laptop-3174729_yiprzu.jpg" alt="">
+                    <img id="profilepic" src="<?php include('path.php');  echo $path . $user['profile_picture'] ?>" alt="">
+                    <?php if ($user_profil === $user_logged) : ?>
+                        <div class="editprofile">Edit Profile</div>
+                    <?php endif; ?>
+
                 </div>
                 <div class="bio">
                     <div class="handle">
-                        <h3>Username</h3>
-                        <span>@</span>
+                        <h3><?php echo $user['username'] ?></h3>
+                        <span><?php echo $user['at_user_name'] ?></span>
                     </div>
                     <p>Bio</p>
-                    <span> <i class="fa fa-location-arrow "></i> Localisation <a href="#"> <i
-                            class="fa fa-birthday-cake" aria-hidden="true"></i> Anniversaire</span>
+                    <span> <i class="fa fa-location-arrow "></i> Localisation <a href="#"> <i class="fa fa-birthday-cake" aria-hidden="true"></i> Anniversaire</span>
                     <br> <span><i class="fa fa-calendar"></i> Date de création (mois/an)</span>
                     <div class="nawa">
                         <div class="followers"><span>Following</span></div>
@@ -59,6 +44,3 @@
             </section>
         </div>
     </div>
-</body>
-
-</html>
