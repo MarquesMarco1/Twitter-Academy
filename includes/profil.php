0df5a995e75f434b2eac889320a95ad4515aff8d
Tony <tony.brechard@epitech.eu>
2024-02-26 16:35:02 +0100

    Template 0.5.8 - Ajout nouveau Profil Utilisateur

--- includes/profil.php
+++ includes/profil.php
@@ -1,3 +1,64 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-?>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Twitter Profile</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='../style/profil.css'>
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <div class="flexcontainer">
+        <div class="middlecontainer">
+            <section class="headsec">
+                <div>
+                    <h3>Username</h2>
+                        <span>Compte Tweets</span>
+                </div>
+            </section>
+            <section class="twitterprofile">
+                <div class="headerprofileimage">
+                    <div id="headerimage">Bannière du mec</div>
+                        <div id="profilepic">Pp du mec</div>
+                    <div class="editprofile">Edit Profile</div>
+                </div>
+                <div class="bio">
+                    <div class="handle">
+                        <h3>Username</h3>
+                        <span>@</span>
+                    </div>
+                    <p>Bio</p>
+                    <span> <i class="fa fa-location-arrow "></i> Localisation <a href="#"> <i
+                            class="fa fa-birthday-cake" aria-hidden="true"></i> Anniversaire</span>
+                    <br> <span><i class="fa fa-calendar"></i> Date de création (mois/an)</span>
+                    <div class="nawa">
+                        <div class="followers"><span>Following</span></div>
+                        <div><span>Followers</span></div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="tweets">
+                <div class="heading">
+                    <p>Tweets</p>
+                    <p>Tweets and Replies</p>
+                    <p>Media</p>
+                    <p>Likes</p>
+                </div>
+            </section>
+            <section class="mytweets">
+                <div class="avi">Pp du mec</div>
+                <div class="tweetbody">
+                    <div>Username, @, date tweet</div>
+                    <div class="tweetcontent">Tweet</div>
+                </div>
+            </section>
+        </div>
+    </div>
+</body>
+
+</html>
