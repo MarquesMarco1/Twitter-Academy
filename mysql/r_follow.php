d4ad543c28b140d9c1e560092bb907fac9d9cb93
Tony <tony.brechard@epitech.eu>
2024-03-07 16:15:19 +0100

    [FIX] (0.8.7) - tu peux pas te follow

--- mysql/r_follow.php
+++ mysql/r_follow.php
@@ -1,7 +1,12 @@
 <?php
 include('mysql.php');
-$sql = $mysqlClient->prepare('INSERT INTO `follow`(`id_user`, `id_follow`, `time`) VALUES (:id_user,:id_follow,now())');
-$sql->execute([
-    "id_user" => $_GET['id_user'],
-    "id_follow" => $_GET['id_follow'],
-]);
+session_start();
+if ($_GET['id_user'] != $_GET['id_follow']) {
+    $sql = $mysqlClient->prepare('INSERT INTO `follow`(`id_user`, `id_follow`, `time`) VALUES (:id_user,:id_follow,now())');
+    $sql->execute([
+        "id_user" => $_GET['id_user'],
+        "id_follow" => $_GET['id_follow'],
+    ]);
+} else {
+    echo "tu peux pas te follow";
+}
