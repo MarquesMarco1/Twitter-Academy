03685ac5073ce72a20f56865f6b67dc9c74aed68
Tony <tony.brechard@epitech.eu>
2024-03-07 16:21:22 +0100

    [FIX] (0.8.8) - tu peux pas follow 2 fois la même personne

--- mysql/r_follow.php
+++ mysql/r_follow.php
@@ -2,11 +2,22 @@
 include('mysql.php');
 session_start();
 if ($_GET['id_user'] != $_GET['id_follow']) {
-    $sql = $mysqlClient->prepare('INSERT INTO `follow`(`id_user`, `id_follow`, `time`) VALUES (:id_user,:id_follow,now())');
+    $sql = $mysqlClient->prepare('SELECT * FROM follow f WHERE f.id_user = :id_user AND f.id_follow = :id_follow');
     $sql->execute([
         "id_user" => $_GET['id_user'],
         "id_follow" => $_GET['id_follow'],
     ]);
+    $follow = $sql->fetch(PDO::FETCH_ASSOC);
+    if (!isset($follow['id_user'])) {
+        $sql = $mysqlClient->prepare('INSERT INTO `follow`(`id_user`, `id_follow`, `time`) VALUES (:id_user,:id_follow,now())');
+        $sql->execute([
+            "id_user" => $_GET['id_user'],
+            "id_follow" => $_GET['id_follow'],
+        ]);
+    } else {
+        echo "tu le follow déjà";
+    }
+ 
 } else {
     echo "tu peux pas te follow";
 }
