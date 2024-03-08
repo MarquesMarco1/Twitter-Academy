680ead6d6545b736d17a3502354f86117d7721c7
Tony <tony.brechard@epitech.eu>
2024-03-08 15:40:20 +0100

    [ADD] (0.9.7) - affichage des tweets avec hashtag

--- tweet/hashtag.php
+++ tweet/hashtag.php
@@ -8,6 +8,32 @@ $sql->execute([
 $hashtag = $sql->fetchAll(PDO::FETCH_ASSOC);
 
 
-foreach ($hashtag as $hashtag) {
-    echo $hashtag['content'] . "<br>";
-}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style/accueil.css">
+</head>
+<body>
+    <?php include('../includes/left-sidebar.php');
+    ?>
+    
+<div class="mise-en-page">
+    <div class="main-content">
+    <?php 
+    foreach ($hashtag as $hashtag) {
+        echo $hashtag['content'] . "<br>";
+    };
+    ?>
+    </div></div>
+    <?php
+    include('../includes/right-sidebar.php');
+    ?>
+    
+</body>
+</html>
