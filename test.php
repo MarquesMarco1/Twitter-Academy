602571646ad4e666ebed2cd1149cc494f3c6a278
Tony <tony.brechard@epitech.eu>
2024-03-08 11:42:10 +0100

    [UPDATE] (0.9.4) - on avance avec les hashtags

--- test.php
+++ test.php
@@ -1,6 +1,11 @@
 <?php
-$ma_varaible_string = 'je test #lamort test.com';
-$trouver = '#';
-$position = strrchr($ma_varaible_string, $trouver);
-$resultat = explode(' ',  substr($position, 1));
-echo $resultat[0];
+include('mysql/mysql.php');
+
+$sql = $mysqlClient->prepare("SELECT t.content  FROM tweet t JOIN hashtag_list hl WHERE t.content LIKE CONCAT('%', '#', hl.hashtag, '%')");
+$sql->execute([]);
+$hashtag = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+
+foreach ($hashtag as $hashtag) {
+    echo $hashtag['content'];
+}
