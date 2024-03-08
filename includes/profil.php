12ddffee6db4a112eb48c4120d5cc1de3acd6ac1
Tony <tony.brechard@epitech.eu>
2024-03-08 10:41:31 +0100

    [UPDATE] (0.9.0) - follower / following fini avec liste

--- includes/profil.php
+++ includes/profil.php
@@ -11,18 +11,28 @@ $sql->execute([
 ]);
 $tweets = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-$sql = $mysqlClient->prepare('SELECT count(u.id) as count FROM user u JOIN follow f ON f.id_user = u.id WHERE u.id = :id');
+$sql = $mysqlClient->prepare('SELECT count(u.id) as count, u.username FROM user u JOIN follow f ON f.id_user = u.id WHERE u.id = :id');
 $sql->execute([
     "id" => $user['id'],
 ]);
 $follower = $sql->fetch(PDO::FETCH_ASSOC);
 
-$sql = $mysqlClient->prepare('SELECT count(u.id) as count FROM user u JOIN follow f ON f.id_follow = u.id WHERE u.id = :id');
+$sql = $mysqlClient->prepare('SELECT count(u.id) as count, u.username FROM user u JOIN follow f ON f.id_follow = u.id WHERE u.id = :id');
 $sql->execute([
     "id" => $user['id'],
 ]);
 $following = $sql->fetch(PDO::FETCH_ASSOC);
 
+$sql = $mysqlClient->prepare('SELECT u.* FROM user u JOIN follow f ON f.id_user = :id WHERE f.id_follow = u.id');
+$sql->execute([
+    "id" => $user['id'],
+]);
+$followingUser = $sql->fetchAll(PDO::FETCH_ASSOC);
+$sql = $mysqlClient->prepare('SELECT u.* FROM user u JOIN follow f ON f.id_follow = :id WHERE f.id_user = u.id');
+$sql->execute([
+    "id" => $user['id'],
+]);
+$followerUser = $sql->fetchAll(PDO::FETCH_ASSOC);
 $sql = $mysqlClient->prepare('SELECT f.* FROM follow f WHERE f.id_user = :id_user AND f.id_follow = :id_follow');
 $sql->execute([
     "id_user" => $_SESSION['USER']['id'],
@@ -59,14 +69,50 @@ $uFollow = $sql->fetch(PDO::FETCH_ASSOC);
                     <br> <span><i class="fa fa-calendar"></i> <?php echo $user['creation_time'] ?> </span>
                     <div class="follow">
                         <div class="followers">
-                            <span><?php echo $follower['count'] ?> Following</span>
+                            <span onclick="togglePopup1()"><?php echo $follower['count'] ?> Following</span>
                         </div>
-                        <div><span><?php echo $following['count'] ?> Followers</span></div>
+                        <div><span onclick="togglePopup2()"><?php echo $following['count'] ?> Followers</span></div>
                     </div>
+
+                    <div id="popup-overlay1">
+                        <div class="popup-content1">
+                            <?php foreach ($followingUser as $followings) : ?>
+                                <div class="followingPopup">
+                                <img src="<?php echo $path . $followings['profile_picture'] ?>" alt="">
+                                <a style='color:blue' href="user_profil.php?id_user=<?php echo $followings['at_user_name'] ?>"><?php echo $followings['at_user_name'] ?></a>
+                                </div>
+                            <?php endforeach; ?>
+                            <a href="javascript:void(0)" onclick="togglePopup1()" class="popup-exit">Fermer</a>
+                        </div>
+                    </div>
+
+                    <div id="popup-overlay2">
+                        <div class="popup-content2">
+                            <?php foreach ($followerUser as $followings) : ?>
+                                <div class="followingPopup">
+                                <img src="<?php echo $path . $followings['profile_picture'] ?>" alt="">
+                                <a style='color:blue' href="user_profil.php?id_user=<?php echo $followings['at_user_name'] ?>"><?php echo $followings['at_user_name'] ?></a>
+                                </div>
+                            <?php endforeach; ?>
+                            <a href="javascript:void(0)" onclick="togglePopup2()" class="popup-exit">Fermer</a>
+                        </div>
+                    </div>
+
+                    <script>
+                        function togglePopup1() {
+                            let popup = document.querySelector('#popup-overlay1');
+                            popup.classList.toggle("open");
+                        }
+
+                        function togglePopup2() {
+                            let popup = document.querySelector('#popup-overlay2');
+                            popup.classList.toggle("open");
+                        }
+                    </script>
 
                     <?php if (isset($uFollow['id_user'])) : ?>
                         <?php if ($uFollow['id_user'] != $user['id']) : ?>
-                        <a href="../mysql/r_follow.php?id_user=<?php echo $_SESSION['USER']['id'] ?>&id_follow=<?php echo $user['id'] ?>">+ Unfollow</a>
+                            <a href="../mysql/r_follow.php?id_user=<?php echo $_SESSION['USER']['id'] ?>&id_follow=<?php echo $user['id'] ?>">+ Unfollow</a>
                         <?php endif; ?>
                     <?php else : ?>
                         <?php if ($uFollow['id_user'] != $user['id']) : ?>
