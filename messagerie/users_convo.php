54af710eaeb8cacabe9fa23d6befe076edc18598
Pau Flk <pauflk@MacBook-Air-de-Pau.local>
2024-03-11 14:55:18 +0100

    ajout d'un début AJAX pour afficher les anciens messages

--- messagerie/users_convo.php
+++ messagerie/users_convo.php
@@ -1,59 +1,65 @@
-<?php 
+<?php
 session_start();
 echo $_GET['id'] . "<br/>";
 
 include('../mysql/mysql.php');
 
-$query = $mysqlClient->prepare("SELECT messages.*, user.username FROM messages JOIN user ON user.id = messages.id_user WHERE id_convo = :id");
-$query->execute([
-    'id' => $_GET['id'],
-]);
-$getConv = $query->fetchAll(PDO::FETCH_ASSOC);
-
-foreach($getConv as $getMsg) {
-    if ($getMsg['id_user'] == $_SESSION['USER']['id']) 
-        echo "Moi : " . $getMsg['content'] . "<br>";
-     else 
-        echo  $getMsg['username'] . " : " . $getMsg['content'] . "<br>";
- 
-}
 ?>
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+    <title>Messagerie</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <script>
-    $(document).ready(async function() {
-      await fetch(tweet)
-
-      async function fetch(tweet) {
-        var tweet = [];
-        async function fetch(tweet) {
-          await $.ajax({
-            url: 'mysql/fetch_tweets.php',
-            type: 'GET',
-            dataType: 'json',
-            success: function(data) {
-              $('#tweets').empty();
-              $.each(data, function(key, tweete) {
-                tweet.push(tweete);
-              });
+        $(document).ready(function() {
+            function chargeMsg() {
+                $.ajax({
+                    url: 'ajax/old_messages.php',
+                    type: 'GET',
+                    dataType: 'json',
+                });
             }
-          });
-        }
-      }
+            chargeMsg();
 
-      tweet = []
-      setInterval(fetch, 10000, tweet);
-    });
+        });
 
+
+
+        // AJAX utilisé pour l'accueil (tweets)
+        /*         $(document).ready(async function() {
+                    await fetch(tweet)
+
+                    async function fetch(tweet) {
+                        var tweet = [];
+                        async function fetch(tweet) {
+                            await $.ajax({
+                                url: 'mysql/fetch_tweets.php',
+                                type: 'GET',
+                                dataType: 'json',
+                                success: function(data) {
+                                    $('#tweets').empty();
+                                    $.each(data, function(key, tweete) {
+                                        tweet.push(tweete);
+                                    });
+                                }
+                            });
+                        }
+                    }
+
+                    tweet = []
+                    setInterval(fetch, 10000, tweet);
+                }); */
     </script>
-</body>
-</html>
 
+</head>
+
+<body>
+
+</body>
+
+</html>
