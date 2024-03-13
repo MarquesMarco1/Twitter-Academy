7543e7273c112b8f63f24ce0bc9beccb9a4a2065
Pau Flk <pauflk@MacBook-Air-de-Pau.local>
2024-03-13 16:58:06 +0100

    [BUG] - Bug de script, arrêt pour aujourd'hui. Continue demain test d'auto-complétion

--- test_dev/script.js
+++ test_dev/script.js
@@ -1,8 +1,12 @@
+const input = document.getElementById("myInput")
+input.onkeydown = myFunction
 function myFunction(e) {
-    if (e.key == "#") {
-        console.log("hastag");
-    }
-    if (e.key == "@") {
-        console.log("arobase");
-    }
+if (e.key == "#") {
+console.log("hastag");
+
+} else if (e.key == "@") {
+console.log("arobase");
+} else {
+console.log("rien");
+}
 }
