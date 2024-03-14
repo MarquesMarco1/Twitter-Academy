191ba32e7011d52c9afd58da9e9bfb1851842642
Pau Flk <pauflk@MacBook-Air-de-Pau.local>
2024-03-14 16:37:05 +0100

    [UPDATE] (0.0.04) - Continuité auto-complétion, beaucoup de problème pour récupérer requête php, maintenant réglé avec xmlhttprequest

--- test_dev/script.js
+++ test_dev/script.js
@@ -1,19 +1,22 @@
-let availableKeywords = [
-    'html',
-    'css',
-    'easy'
-];
+let result = []
 
-const resultBox = document.querySelector(".result-box");
-const inputBox = document.getElementById("input-box");
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            const response = JSON.parse(xhttp.responseText);
+           console.log(response);
+        }
+    };
+    xhttp.open("GET", "users.php", true);
+    xhttp.send();
+
+    const resultBox = document.querySelector(".result-box");
+    const inputBox = document.getElementById("input-box");
 
 inputBox.onkeyup = function () {
-    let result = [];
     let input = inputBox.value;
     if (input.length) {
-        result = availableKeywords.filter((keyword) => {
-            return keyword.toLowerCase().includes(input.toLowerCase());
-        });
-        console.log(result)
-    }
-}
+
+
+
+    }}
