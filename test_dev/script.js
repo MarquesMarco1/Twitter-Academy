8cbc89b8002d90d3ef3ebbeec09ffb6363e87dfd
Pau Flk <pauflk@MacBook-Air-de-Pau.local>
2024-03-15 13:14:54 +0100

    [UPDATE] (0.0.08) - Essai d'une condition pour effacer ul quand il n'y a plus de @ + ajout de la fonction complete() qui sert à écrire sur quoi on a cliqué

--- test_dev/script.js
+++ test_dev/script.js
@@ -23,21 +23,37 @@ inputBox.onkeyup = function () {
         console.log(arobase);
         if (arobase.startsWith('@')) {
             console.log('oui')
-        let result = [];
-        if (arobase.length) {
-            result = userName.filter((keyword) => {
-                return keyword.toLowerCase().includes(arobase.toLowerCase())
-            });
-            console.log(result);
+            let result = [];
+            if (arobase.length) {
+                result = userName.filter((keyword) => {
+                    return keyword.toLowerCase().includes(arobase.toLowerCase())
+                });
+                console.log(result);
+
+            }
+            display(result);
+
+            if (!arobase.length) {
+                resultBox.innerHTML = '';
+            }
+        } else {
+            resultBox.innerHTML = '';
         }
-        display(result);
-    }}
+    }
 }
 
 function display(result) {
-    const content = result.map((list) => {
-        return "<li>" + list + "</li>";
-    });
+    let input = inputBox.value;
+    if (input.startsWith('@') || input.includes('@')) {
+        const content = result.map((list) => {
+            return "<li onclick=complete(this)>" + list + "</li>";
+        }).join('');
 
-    resultBox.innerHTML = "<ul>" + content + "</ul>";
+        resultBox.innerHTML = "<ul>" + content + "</ul>";
+    }
+}
+
+function complete(list) {
+    inputBox.value = list.innerHTML;
+    resultBox.innerHTML = '';
 }
