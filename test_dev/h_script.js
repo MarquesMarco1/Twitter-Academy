eb7c4daea5bdbb992bdb86e31a965709426f8a91
Pau Flk <pauflk@air-de-pau.home>
2024-03-16 13:44:49 +0100

    [UPDATE] (0.0.11) - Ajout auto-complétion des hastags

---
+++ test_dev/h_script.js
@@ -0,0 +1,73 @@
+let hashtagName = []
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        const response = JSON.parse(xhttp.responseText);
+        hashtagName = response.map(hashtag_list => hashtag_list.hashtag);
+        console.log(hashtagName);
+    }
+};
+xhttp.open("GET", "hashtag.php", true);
+xhttp.send();
+
+
+const resultBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function () {
+    let input = inputBox.value;
+    if (input.startsWith('#')) {
+        let search = input.indexOf('#');
+        let arobase = input.substring(search);
+        console.log(arobase);
+        if (arobase.startsWith('#')) {
+            console.log('oui')
+            let result = [];
+            if (arobase.length) {
+                result = hashtagName.filter((keyword) => {
+                    return keyword.toLowerCase().includes(arobase.toLowerCase())
+                });
+                console.log(result);
+
+            }
+            display(result);
+
+
+        }
+    } else if (input.includes('#')) {
+        let search = input.indexOf('#');
+        let arobase = input.substring(search);
+        console.log(arobase);
+        if (arobase.startsWith('#')) {
+            console.log('oui')
+            let result = [];
+            if (arobase.length) {
+                result = hashtagName.filter((keyword) => {
+                    return keyword.toLowerCase().includes(arobase.toLowerCase())
+                });
+                console.log(result);
+
+            }
+            display(result);
+        }
+    } else {
+        resultBox.innerHTML = "";
+    }
+}
+
+function display(result) {
+    let input = inputBox.value;
+    if (input.startsWith('#') || input.includes('#')) {
+        const content = result.map((list) => {
+            return "<li onclick=complete(this)>" + list + "</li>";
+        }).join('');
+
+        resultBox.innerHTML = "<ul>" + content + "</ul>";
+    }
+}
+
+function complete(list) {
+    inputBox.value = inputBox.value.replace("#", "") + list.innerHTML;
+    resultBox.innerHTML = '';
+} 
